30e780c0a84c1059b26774f9711fb9610f10394e
kanera <kaner.assaf@gmail.com>
2023-05-11 18:24:17 -0500

    functioning image carousel

--- packages/client/src/components/MainIntervalPictures.tsx
+++ packages/client/src/components/MainIntervalPictures.tsx
@@ -9,15 +9,20 @@ function FeaturedPicturesContainer(props: IFeaturedPicturesContainerProps) {
     const time = 3500;
 
     useEffect(() => {
-        console.log("inside")
+        console.log("inside", currentPicture)
         const next = (currentPicture + 1) % props.featuredPicturesPaths.length;
         const id = setTimeout(() => setCurrentPicture(next), time);
         return () => clearTimeout(id);
     }, [currentPicture, props.featuredPicturesPaths]);
 
+    const onRightClick = () => setCurrentPicture(prev => prev === props.featuredPicturesPaths.length - 1 ? 0 : prev + 1);
+    const onLeftClick = () => setCurrentPicture(prev => prev === 0 ? props.featuredPicturesPaths.length - 1 : prev - 1);
+
     return (
         <div className="mainContainer">
-            <img height="350px" width="650px" src={props.featuredPicturesPaths[currentPicture]} alt="some picture" />
+            <img className="image" height="350px" width="650px" src={props.featuredPicturesPaths[currentPicture]} alt="some picture" />
+            <div className="rightArrow" onClick={onRightClick}>{"-->"}</div>
+            <div className="leftArrow" onClick={onLeftClick}>{"<--"}</div>
         </div>
     );
 }
