1ff9c6acfa259dec9e0c44e8dcae566a4213b1ad
kanera <kaner.assaf@gmail.com>
2023-05-11 18:15:53 -0500

    add router to application

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import routes from './routes';
+// import Logo from './components/Logo';
+import Navbar from './components/Navbar';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={`page-${index}`} path={route.path} Component={route.main} />
+        ))}
+      </Routes>
     </div>
   );
 }
