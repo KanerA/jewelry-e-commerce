eac02b7c6c69083931d74c67c6a79614b75fdc2e
kanera <kaner.assaf@gmail.com>
2023-05-12 16:41:06 -0500

    change featured image to style instead of img tag

--- packages/client/src/components/IntervalCarousel.tsx
+++ packages/client/src/components/IntervalCarousel.tsx
@@ -7,6 +7,16 @@ interface IFeaturedPicturesContainerProps {
 function FeaturedPicturesContainer(props: IFeaturedPicturesContainerProps) {
     const [currentPicture, setCurrentPicture] = useState<number>(0);
     const time = 3500;
+    const onRightClick = () => setCurrentPicture(prev => prev === props.featuredPictures.length - 1 ? 0 : prev + 1);
+    const onLeftClick = () => setCurrentPicture(prev => prev === 0 ? props.featuredPictures.length - 1 : prev - 1);
+
+    const containerStyleImage = {
+        height: '80vh', // override in css file
+        width: '80vw', // override in css file
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${props.featuredPictures[currentPicture]})`
+    }
 
     useEffect(() => {
         console.log("inside", currentPicture)
@@ -15,30 +25,24 @@ function FeaturedPicturesContainer(props: IFeaturedPicturesContainerProps) {
         return () => clearTimeout(id);
     }, [currentPicture, props.featuredPictures]);
 
-    const onRightClick = () => setCurrentPicture(prev => prev === props.featuredPictures.length - 1 ? 0 : prev + 1);
-    const onLeftClick = () => setCurrentPicture(prev => prev === 0 ? props.featuredPictures.length - 1 : prev - 1);
 
     return (
-        <div className="mainContainer">
-            <img
-                className="image"
-                height="350px"
-                width="650px"
-                src={props.featuredPictures[currentPicture]}
-                alt="some picture"
-            />
-            <div className="rightArrow" onClick={onRightClick}>{"-->"}</div>
-            <div className="leftArrow" onClick={onLeftClick}>{"<--"}</div>
-            <div className="imageIndicatorContainer">
-                {
-                    props.featuredPictures.map((_, index) => {
-                        // ? maybe add functionality of choosing which picture to show
-                        if (currentPicture === index) return <>1</> // TODO: change to icons
-                        return <>0</> // TODO: change to icons
-                    })
-                }
-            </div>
-        </div>
+        <>
+            KAMA Jewelry
+            <div className="mainContainer" style={containerStyleImage}>
+                <div className="rightArrow" onClick={onRightClick}>{"-->"}</div>
+                <div className="leftArrow" onClick={onLeftClick}>{"<--"}</div>
+                <div className="imageIndicatorContainer">
+                    {
+                        props.featuredPictures.map((_, index) => {
+                            // ? maybe add functionality of choosing which picture to show
+                            if (currentPicture === index) return <>1</> // TODO: change to icons
+                            return <>0</> // TODO: change to icons
+                        })
+                    }
+                </div>
+            </div >
+        </>
     );
 }
 
