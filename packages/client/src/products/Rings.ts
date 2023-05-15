480b267fe4521233132bf24d9375f121c835149c
kanera <kaner.assaf@gmail.com>
2023-05-15 17:10:11 -0500

    config files for product type , prices, images etc.

---
+++ packages/client/src/products/Rings.ts
@@ -0,0 +1,12 @@
+import { TSingleGalleryProduct } from "../components/ProductsGallerySingleItem";
+
+const RINGS: TSingleGalleryProduct[] = [
+    {
+        name: "טבעת עמית שחורה",
+        description: "טבעת עמית שחורה",
+        imageSrc: "images/productImages/20230423131657_IMG_5266.jpg",
+        price: 10
+    }
+];
+
+export default RINGS;
