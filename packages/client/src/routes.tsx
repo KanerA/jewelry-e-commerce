953f4f0825403195a97fba931ec0445683a6b567
kanera <kaner.assaf@gmail.com>
2023-05-15 17:42:49 -0500

    add paths for all type of products

--- packages/client/src/routes.tsx
+++ packages/client/src/routes.tsx
@@ -4,6 +4,9 @@ import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import ProductType from "./pages/ProductType";
 import RINGS from './products/Rings';
+import BRACELETS from "./products/bracelets";
+import EARRINGS from "./products/earrings";
+import NECKLACES from "./products/necklaces";
 
 type routeType = {
   path: string;
@@ -34,8 +37,23 @@ const routes: routeType[] = [
   },
   {
     path: "/rings",
-    main: () => <ProductType products={RINGS} />,
+    main: () => <ProductType productType={"Rings"} products={RINGS} />,
     sidebar: () => <div>Rings!</div>
+  },
+  {
+    path: "/bracelets",
+    main: () => <ProductType productType={"Bracelets"} products={BRACELETS} />,
+    sidebar: () => <div>Bracelets!</div>
+  },
+  {
+    path: "/earrings",
+    main: () => <ProductType productType={"Earrings"} products={EARRINGS} />,
+    sidebar: () => <div>Earrings!</div>
+  },
+  {
+    path: "/necklaces",
+    main: () => <ProductType productType={"Necklaces"} products={NECKLACES} />,
+    sidebar: () => <div>Necklaces!</div>
   }
 ]
 
