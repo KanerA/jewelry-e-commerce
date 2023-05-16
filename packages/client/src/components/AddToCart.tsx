5992b97c6b7abb37ec2279e5811d932bf1c7a8fc
kanera <kaner.assaf@gmail.com>
2023-05-15 19:11:20 -0500

    add to cart button and fav icon

---
+++ packages/client/src/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddToCart = () => {
+    return (
+        <div className="addToCartContainer">
+            <button>Add To Cart</button>
+        </div>
+    );
+};
+
+export default AddToCart;
---
+++ packages/client/src/components/FavoriteIcon.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { MdFavorite, MdOutlineFavoriteBorder } from "react-icons/md";
+
+interface IFavoriteIconProps {
+    isFavorite: boolean;
+    onClick: (props: any) => void;
+}
+
+const FavoriteIcon = ({ isFavorite, onClick }: IFavoriteIconProps) => {
+    return (
+        <div className="favoriteIcon" onClick={onClick}>
+            {isFavorite ? <MdFavorite color='#FF4643' /> : <MdOutlineFavoriteBorder />}
+        </div>
+    );
+};
+
+export default FavoriteIcon;
