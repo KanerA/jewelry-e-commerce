43bed9605cbae084e4fdf5b322cc0000b7a9a84b
kanera <kaner.assaf@gmail.com>
2023-05-15 19:06:25 -0500

    fav icon, add to cart comp, format

--- packages/client/src/components/ProductsGallerySingleItem.tsx
+++ packages/client/src/components/ProductsGallerySingleItem.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PriceTag from './PriceTag';
+import FavoriteIcon from './FavoriteIcon';
+import AddToCart from './AddToCart';
 
 export type TSingleGalleryProduct = {
     imageSrc: string; // image path inside src folder
@@ -10,17 +12,24 @@ export type TSingleGalleryProduct = {
 }
 
 const ProductsGallerySingleItem = (props: TSingleGalleryProduct) => {
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
+    const onFavoriteClick = (props: any) => setIsFavorite(prev => !prev);
     return (
         <div className="gallerySingleItem">
             <div className="imageContainer"> {/* TODO: change alt prop */}
-                <img className="singleImage" style={{
-                    width: "150px",
+                <img
+                    className="singleImage"
+                    style={{
+                        width: "150px",
 
-                }} src={require("../" + props.imageSrc)} alt="some picture" />
+                    }}
+                    src={require("../" + props.imageSrc)} alt="some picture"
+                />
+                <FavoriteIcon onClick={onFavoriteClick} isFavorite={isFavorite} />
             </div>
             <div className="itemName">{props.name}</div>
             <PriceTag price={props.price} />
-            <div className="itemDescription">{props.description}</div>
+            <AddToCart />
         </div>
     );
 };
