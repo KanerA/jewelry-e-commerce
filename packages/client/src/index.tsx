d89abce38dbd3a6bf36e16d0674969c562c7fd33
kanera <kaner.assaf@gmail.com>
2023-05-17 14:30:25 -0500

    calmer bg color

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import "./styles/index.css"
 
 import App from './App';
 import store from './store';
