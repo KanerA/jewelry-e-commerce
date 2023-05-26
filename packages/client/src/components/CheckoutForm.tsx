4096db5bdbafacd895989f6e8ea32fdbca98570b
kanera <kaner.assaf@gmail.com>
2023-05-25 22:28:39 -0500

    checkout form fixes

--- packages/client/src/components/CheckoutForm.tsx
+++ packages/client/src/components/CheckoutForm.tsx
@@ -3,23 +3,22 @@ import { useForm } from 'react-hook-form';
 
 const CheckoutForm = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (data: any) => console.log(data);
-    console.log(errors);
+    const onSubmit = (data: any) => console.log("form data", { data, timestamp: new Date() });
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
-            <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
+            <input type="text" placeholder="First name" {...register("FirstName", { required: true, maxLength: 30 })} />
+            <input type="text" placeholder="Last name" {...register("LastName", { required: true, maxLength: 30 })} />
             <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-            <input type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, maxLength: 10 })} />
+            <input type="tel" placeholder="Mobile number" {...register("MobileNumber", { required: true, maxLength: 10 })} />
             <select {...register("Title", { required: true })}>
                 <option value="Mr">Mr</option>
                 <option value="Mrs">Mrs</option>
                 <option value="Miss">Miss</option>
                 <option value="Dr">Dr</option>
             </select>
-            <input type="text" placeholder="Street Name" {...register("Street Name", {})} />
-            <input type="text" placeholder="Apartment Number" {...register("Apartment Number", {})} />
+            <input type="text" placeholder="Street Name" {...register("StreetName", {})} />
+            <input type="text" placeholder="Apartment Number" {...register("ApartmentNumber", {})} />
             <input type="text" placeholder="Country" {...register("Country", {})} />
 
             <input type="submit" />
