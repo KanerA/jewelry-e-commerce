1c3669144e99714d6b6bc256996a9e419ce681ef
kanera <kaner.assaf@gmail.com>
2023-05-25 22:21:00 -0500

    small changes

--- packages/client/src/hooks/useAddToCart.ts
+++ packages/client/src/hooks/useAddToCart.ts
@@ -1,12 +1,10 @@
 import { useDispatch } from "react-redux";
 import commerce from "../lib/commerce";
-import { setProductsData } from "../store/actions";
 
 const useAddToCart = () => {
     const dispatch = useDispatch();
     return async (id: string, qty: number) => {
-        await commerce.cart.add(id, qty)
-        // dispatch(addProductToCart());
+        await commerce.cart.add(id, qty);
     }
 };
 
