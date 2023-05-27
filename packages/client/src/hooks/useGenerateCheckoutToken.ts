b1466bf05e5b7ba6d7c565458a41ba1bab84d0f8
kanera <kaner.assaf@gmail.com>
2023-05-27 16:44:05 -0500

    check generate checkout token response

--- packages/client/src/hooks/useGenerateCheckoutToken.ts
+++ packages/client/src/hooks/useGenerateCheckoutToken.ts
@@ -5,9 +5,13 @@ import { setCheckoutToken } from "../store/actions";
 const useGenerateCheckoutToken = (identifier: string) => {
     const dispatch = useDispatch();
     return async () => {
-        const res = await commerce.checkout.generateToken(identifier, { type: "cart" });
-        console.log(res);
-        dispatch(setCheckoutToken(res.id))
+        try {
+            const res = await commerce.checkout.generateToken(identifier, { type: "cart" });
+            console.log(res);
+            dispatch(setCheckoutToken(res.id))
+        } catch (err: any) {
+            if (err.statusCode === 422) console.log("Cart is empty")
+        }
     }
 };
 
