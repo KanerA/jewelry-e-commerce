3022262b9ca4714863a0191f77deefe3bceb1719
kanera <kaner.assaf@gmail.com>
2023-05-27 16:16:47 -0500

    joined to last

--- packages/client/src/hooks/useRemoveFromCart.ts
+++ packages/client/src/hooks/useRemoveFromCart.ts
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux";
 import commerce from "../lib/commerce";
-import { setProductsData } from "../store/actions";
+import { actionRemoveItemFromCart } from "../store/actions";
 
 const useRemoveFromCart = () => {
     const dispatch = useDispatch();
     return async (id: string) => {
         await commerce.cart.remove(id);
-        // dispatch(removeFromCart(id));
+        dispatch(actionRemoveItemFromCart(id));
     }
 };
 
