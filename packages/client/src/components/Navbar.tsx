5b831c6c4902b9c0fe6e673c74d61c23f10bba91
kanera <kaner.assaf@gmail.com>
2023-06-06 12:09:58 -0500

    start trying tailwind

--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -5,14 +5,15 @@ import Search from './Search';
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <Link to="/"><Logo /></Link>
-            <ul className="pageLinks">
-                <li><Link to="/about">about</Link></li>
-                <li><Link to="/cart">cart</Link></li>
-                <li><Link to="/favorites">favorites</Link></li>
-            </ul>
-            <Search />
+        <nav className="navbar bg-green border-gray-200 dark:bg-gray-900">
+            <div className="bg-green max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link to="/"><Logo /></Link>
+                <ul className="pageLinks flex justify-evenly bg-green">
+                    <li><Link to="/about"><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">about</span></Link></li>
+                    <li><Link to="/cart"><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">cart</span></Link></li>
+                    <li><Link to="/favorites"><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">favorites</span></Link></li>
+                </ul>
+            </div>
         </nav>
     );
 }
