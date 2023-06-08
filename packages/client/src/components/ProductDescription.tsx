b815894c886e5752e0873e4c3009aaa5bb1f36b1
kanera <kaner.assaf@gmail.com>
2023-06-08 18:57:21 -0500

    .

--- packages/client/src/components/ProductDescription.tsx
+++ packages/client/src/components/ProductDescription.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProductDescription = ({ text }: { text: string }) => {
     return (
         <div className="productDescription">
-            <p>{text}</p>
+            {text}
         </div>
     );
 };
